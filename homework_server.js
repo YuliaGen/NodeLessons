8d01ff026904ba6b90bc8e90d8c54f65fae73fc5
Julia <petrovayuliavik@gmail.com>
2024-04-20 21:09:24 +0300

    fixed remarck

--- homework_server.js
+++ homework_server.js
@@ -6,38 +6,34 @@ let counter2 = 0;
 let counter3 = 0;
 
 const server = http.createServer((req, res) => {
+    res.setHeader("Content-Type", "text/html; charset=utf-8;");
    
 if(req.url === '/') {
-    counter1 = counter1+1;
-res.writeHead(200, {
-'Content-Type': 'text/html; charset=UTF-8'
-});
-
-res.end(
-    `<h1>Первая страница </h1>
-    <h2> <a href="http://127.0.0.1:3000/about"> для перехода на страницу 2 </a></h2> 
+    counter1 ++;
+res.write(
+`<h1>Первая страница </h1>
+    <h2> <a href="/about"> для перехода на страницу 2 </a></h2> 
     <br> 
     <p>Вы зашли на страницу ${counter1} раз</p>`);
 
 } else if (req.url === '/about') {
-    counter2++;
-res.writeHead(200, {
-'Content-Type': 'text/html; charset=UTF-8'
-});
-
-res.end(
-    `<h1> Вторая страница </h2>
-    <h2><a href="http://127.0.0.1:3000"> На первую страницу </a></h2>
+    counter2 ++;
+res.write(
+`<h1> Вторая страница </h2>
+    <h2><a href="/"> На первую страницу </a></h2>
             <br> 
     <p>Вы зашли на страницу ${counter2} раз</p>`);
+
+
 } else {
-res.writeHead(404, {
-'Content-Type': 'text/html; charset=UTF-8'
-});
-counter3 ++;
-res.end(`<h1>Такой страницы не существует</h1>
+    counter3 ++;
+res.write(
+`<h1>Такой страницы не существует</h1>
 <p>Вы зашли на страницу ${counter3}раз </p>`);
+
 }
+
+res.end();
 });
 
 const port = 3000;
